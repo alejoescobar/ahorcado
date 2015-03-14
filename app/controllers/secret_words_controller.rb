dd569925a3e8e1312ac2b1fda459c12c7a55649a
Alejandro Escobar <alejoescobac@gmail.com>
2015-03-14 15:59:22 -0500

    Add win message and validation

--- app/controllers/secret_words_controller.rb
+++ app/controllers/secret_words_controller.rb
@@ -2,21 +2,22 @@ class SecretWordsController < ApplicationController
   @@letters = []
   @@correct_letter = []
   @@incorrect_letter = @@letters - @@correct_letter
-  @@secret = "murcielago"
-  @@unknown_letters = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_",]
+  @@secret = "banano"
+  @@unknown_letters = ["_", "_", "_", "_", "_", "_"]
 
   def index
     @correct_letter = @@correct_letter
     @incorrect_letter = @@incorrect_letter
     @letters = @@letters
     @unknown_letters = @@unknown_letters
+    @secret = @@secret
   end
+
   def recibe
     @@secret.chars.each_with_index do |character, index|
       if params[:letter] == character
         @@correct_letter.push(params[:letter])
         @@unknown_letters[index] = params[:letter]
-        break
       end 
     end
     @@letters << params[:letter]
@@ -27,7 +28,7 @@ class SecretWordsController < ApplicationController
   def new
     @@correct_letter.clear
     @@incorrect_letter.clear
-    @@unknown_letters = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_",]
+    @@unknown_letters = ["_", "_", "_", "_", "_", "_"]
     @@letters.clear
     redirect_to secret_words_path
   end
